4b52f313e869f4895e544940ecaa97673df56598
Robin Menzenbach <robin@menzenbachs.de>
2025-07-30 21:07:24 +0200

    Sets up initial project structure
    Initializes project with editorconfig, gitignore, pre-commit config, and basic CI/CD workflows.
    
    Adds a basic example node and tests, laying the foundation for future development.

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for alignboi."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """AlignBoi"""
+__email__ = "hi@adwerk.ai"
+__version__ = "0.0.1"
+
+from .src.alignboi.nodes import NODE_CLASS_MAPPINGS
+from .src.alignboi.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
